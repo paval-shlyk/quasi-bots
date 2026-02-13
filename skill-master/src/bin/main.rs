a4a3217219f1f9fdee2d5ccb5db20205c3a8ee24
Paval Shlyk <sovetskiusoyos@gmail.com>
2026-02-13 13:28:12 +0100

    ci: add release job, update lint job, update deb package definition

--- skill-master/src/bin/main.rs
+++ skill-master/src/bin/main.rs
@@ -1,7 +1,21 @@
+use clap::Parser;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Path to the configuration file
+    #[arg(short, long, default_value = "config.toml")]
+    config: String,
+    /// Port to run the server on
+    #[arg(short, long, default_value = "8080")]
+    port: u16,
+}
 
 #[tokio::main]
 pub async fn main() {
+    let args = Args::parse();
+
     tracing_subscriber::registry()
         .with(
             tracing_subscriber::EnvFilter::try_from_default_env()
@@ -10,9 +24,11 @@ pub async fn main() {
         .with(tracing_subscriber::fmt::layer())
         .init();
 
-    let raw_config = tokio::fs::read_to_string("config.toml")
+    let raw_config = tokio::fs::read_to_string(&args.config)
         .await
-        .expect("Failed to read config file");
+        .unwrap_or_else(|_| {
+            panic!("Failed to read config file at {}", args.config)
+        });
 
     let config: skill_master::Config =
         toml::from_str(&raw_config).expect("Failed to parse config");
@@ -28,8 +44,9 @@ pub async fn main() {
 
     let app = skill_master::routes::create_routes(state);
 
-    let addr: std::net::SocketAddr =
-        "0.0.0.0:8080".parse().expect("Failed to parse address");
+    let addr: std::net::SocketAddr = format!("0.0.0.0:{}", args.port)
+        .parse()
+        .expect("Failed to parse address");
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
     tracing::info!("Starting server on {}", addr);
 
